11ecaaa530e4f10288d865ba529458491c1e1f36
Andy Gnias <AGnias47@gmail.com>
2020-09-12 14:37:11 -0400

    Framework of app

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const axios = require("axios");
 const config = require("./config.json");
 
 
-const meals = ["lasagna"];
+async function getMeals(meals) {
+    meals.forEach(async function (meal) {
+        let data = await axios.get(config.api_base_url + "search.php?s=" + meal)
+        .then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            throw Error(error);
+        });
+    });
+};
 
-meals.forEach(async function (meal) {
-    let data = await axios.get(config.api_base_url + "search.php?s=" + meal)
+
+async function getRandomMeal() {
+    let data = await axios.get(config.api_base_url + "random.php")
     .then(function (response) {
         console.log(response.data);
     }).catch(function (error) {
         throw Error(error);
     });
-});
+};
+
+
+getMeals(["lasagna"]);
+getRandomMeal();
