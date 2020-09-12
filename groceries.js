075da16e4b19249dd37903dfca7ab1864771fda5
Andy Gnias <AGnias47@gmail.com>
2020-09-12 15:15:58 -0400

    Running test

--- groceries.js
+++ groceries.js
@@ -3,43 +3,45 @@
  *
  * @module groceries
  */
-
 const axios = require("axios");
+
 const config = require("./config.json");
 
 module.exports = {
-    /**
-     * Gets meals data from a list of meals
-     *
-     * @param meals {array}   List of strings
-     * @returns {array}       List of meal objects
-     *
-     * @example
-     * getMeals(["lasagna", "pork tacos", "tiramisu"]);
-     */
-    getMeals: async function (meals) {
-        let mealData = [];
-        for (let meal of meals) {
-            try {
-                const response = await axios.get(config.api_base_url + "search.php?s=" + meal);
-                mealData.push(response.data);
-            } catch (error) {
-                throw Error(error);
-            }
-        }
-        return mealData;
-    },
+  /**
+   * Gets meals data from a list of meals
+   *
+   * @param meals {array}   List of strings
+   * @returns {array}       List of meal objects
+   *
+   * @example
+   * getMeals(["lasagna", "pork tacos", "tiramisu"]);
+   */
+  getMeals: async function (meals) {
+    let mealData = [];
 
-    /**
-     * Gets data for a random meal
-     *
-     * @returns {Object} Meal data
-     *
-     * @example
-     * getRandomMeal();
-     */
-    getRandomMeal: async function () {
-        const response = await axios.get(config.api_base_url + "random.php");
-        return response.data;
-    },
-}
+    for (let meal of meals) {
+      try {
+        const response = await axios.get(config.api_base_url + "search.php?s=" + meal);
+        mealData.push(response.data);
+      } catch (error) {
+        throw Error(error);
+      }
+    }
+
+    return mealData;
+  },
+
+  /**
+   * Gets data for a random meal
+   *
+   * @returns {Object} Meal data
+   *
+   * @example
+   * getRandomMeal();
+   */
+  getRandomMeal: async function () {
+    const response = await axios.get(config.api_base_url + "random.php");
+    return response.data;
+  }
+};
---
+++ test/test.js
@@ -0,0 +1,15 @@
+const groceries = require("../groceries.js");
+
+const {
+  expect
+} = require("chai");
+
+const mocha = require("mocha");
+
+describe("Groceries", async function () {
+  it("Console print test", async function () {
+    let meals = await groceries.getMeals(["lasagna"]);
+    console.log(meals[0]);
+    console.log(await groceries.getRandomMeal());
+  });
+});
