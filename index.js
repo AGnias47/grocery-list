226d1fe624094898b646e7fc6e3f462973737c1b
Andy Gnias <AGnias47@gmail.com>
2020-09-12 15:29:07 -0400

    babelify

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const groceries = require("./groceries.js");
 
 async function main() {
-    let meals = await groceries.getMeals(["lasagna"]);
-    console.log(meals[0]);
-    console.log(await groceries.getRandomMeal());
+  let meals = await groceries.getMeals(["lasagna"]);
+  console.log(meals[0]);
+  console.log(await groceries.getRandomMeal());
 }
 
 main();
