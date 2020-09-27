ead83466eb6cdf2b306cb12fa09c92677ba9b078
Andy Gnias <AGnias47@gmail.com>
2020-09-27 19:10:44 -0400

    Update formatting options Improve tests

--- groceries.js
+++ groceries.js
@@ -21,7 +21,7 @@ module.exports = {
    * @example
    * getMealData("pork tacos");
    */
-  getMealData: async function (meal) {
+  getMealData: async function(meal) {
     const response = await axios.get(config.api_base_url + "search.php?s=" + meal);
     return new Meal(response.data.meals[0]);
   },
@@ -34,7 +34,7 @@ module.exports = {
    * @example
    * getRandomMeal();
    */
-  getRandomMeal: async function () {
+  getRandomMeal: async function() {
     const response = await axios.get(config.api_base_url + "random.php");
     return new Meal(response.data.meals[0]);
   }
--- test/test.js
+++ test/test.js
@@ -8,10 +8,20 @@ const {
 
 const mocha = require("mocha");
 
-describe("Groceries", async function () {
-  it("Console print test", async function () {
-    let meal = await groceries.getMealData("lasagna");
-    console.log(meal);
+describe("Groceries", async function() {
+  it("Console print test", async function() {
     console.log(await groceries.getRandomMeal());
   });
+
+  it("Get specific meal data", async function() {
+    const mealName = "lasagna";
+    let meal = await groceries.getMealData(mealName);
+    expect(meal.name.toLowerCase()).to.include(mealName);
+  });
+
+  it("Get random meal data", async function() {
+    let randomMeal = await groceries.getRandomMeal();
+    expect(randomMeal.name).to.be.a("string");
+  });
 });
+
