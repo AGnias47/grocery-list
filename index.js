65eb450bece13b2af061b1ef9a00aafeeea966c3
Andy Gnias <AGnias47@gmail.com>
2020-09-27 20:24:17 -0400

    Simplest implementation working with hard-coded meals

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const groceries = require("./groceries.js");
 'use strict';
 
 async function main() {
-  let meal = await groceries.getMealData("lasagna");
-  console.log(meal);
-  console.log(await groceries.getRandomMeal());
+  let meals = ["lasagna", "tacos", "steak"];
+  let ingredients = [];
+  for (let meal of meals) {
+    let mealData = await groceries.getMealData(meal);
+    ingredients = ingredients.concat(mealData.ingredients);
+  }
+  ingredients = groceries.filterIngredients(ingredients);
+  console.log(ingredients);
 }
 
 main();
